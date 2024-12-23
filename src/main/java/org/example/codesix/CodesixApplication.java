71fad3f20500953ef598fb156d6e7b275f0f5116
heroKim997 <ghkufud6545@gmail.com>
2024-12-23 21:54:37 +0900

    initial commit

---
+++ src/main/java/org/example/codesix/CodesixApplication.java
@@ -0,0 +1,13 @@
+package org.example.codesix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodesixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/codesix/CodesixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.codesix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
