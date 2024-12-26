5e2331f56ce5d93bd4c5b6cd9c9088d2bb196c0b
정가현 <kahyun0608@gmail.com>
2024-12-27 00:19:22 +0900

    fix : /api 경로 추가

--- src/main/java/org/example/codesix/global/config/SecurityConstants.java
+++ src/main/java/org/example/codesix/global/config/SecurityConstants.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class SecurityConstants {
     public static final List<String> WHITE_LIST = List.of(
-            "/users/signup",
-            "/users/login"
+            "/api/users/signup",
+            "/api/users/login"
     );
 }
