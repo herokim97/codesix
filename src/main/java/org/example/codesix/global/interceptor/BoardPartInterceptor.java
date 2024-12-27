454612fa974c602fdf0d1fbfabf0b5c7f7730ca5
정가현 <kahyun0608@gmail.com>
2024-12-27 23:53:29 +0900

    Feat : BoardPartInterceptor 틀 구현

---
+++ src/main/java/org/example/codesix/global/interceptor/BoardPartInterceptor.java
@@ -0,0 +1,8 @@
+package org.example.codesix.global.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class BoardPartInterceptor implements HandlerInterceptor {
+}
