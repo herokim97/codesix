68109bde14992bdb46ed7acaf11eba104bf0b16d
정가현 <kahyun0608@gmail.com>
2024-12-27 21:23:38 +0900

    Feat : Type enum 추가

---
+++ src/main/java/org/example/codesix/domain/notification/Type.java
@@ -0,0 +1,20 @@
+package org.example.codesix.domain.notification;
+
+public enum Type {
+    BOARD_ADD,
+    BOARD_UPDATE,
+    BOARD_DELETE,
+    LIST_ADD,
+    LIST_UPDATE,
+    LIST_DELETE,
+    CARD_ADD,
+    CARD_UPDATE,
+    CARD_DELETE,
+    MEMBER_ADD,
+    MEMBER_DELETE,
+    CARD_MEMBER_ADD,
+    CARD_MEMBER_DELETE,
+    COMMENT_ADD,
+    COMMENT_UPDATE,
+    COMMENT_DELETE;
+}
