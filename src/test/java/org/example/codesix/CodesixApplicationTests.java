773acb05271c4512cf050d2f8375ee883431294d
heroKim997 <ghkufud6545@gmail.com>
2024-12-29 23:43:32 +0900

    fix. 실행 파일 및 테스트 파일 수정

--- src/test/java/org/example/codesix/CodesixApplicationTests.java
+++ src/test/java/org/example/codesix/CodesixApplicationTests.java
@@ -6,4 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodesixApplicationTests {
 
+    @Test
+    void contextLoads() {
+    }
+
 }
--- src/test/java/org/example/codesix/domain/user/controller/UserControllerTest.java
+++ src/test/java/org/example/codesix/domain/user/controller/UserControllerTest.java
@@ -1,60 +1,60 @@
-//package org.example.codesix.domain.user.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.example.codesix.domain.user.dto.*;
-//import org.example.codesix.domain.user.entity.User;
-//import org.example.codesix.domain.user.enums.UserRole;
-//import org.example.codesix.domain.user.repository.UserRepository;
-//import org.example.codesix.domain.user.service.UserService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class UserControllerTest {
-//
-//    private MockMvc mockMvc;
-//
-//    @Mock
-//    private UserService userService;
-//
-//    @InjectMocks
-//    private UserController userController;
-//
-//    private ObjectMapper objectMapper;
-//
-//    @Mock
-//    private UserRepository userRepository;
-//
-//    @Mock
-//    private BCryptPasswordEncoder passwordEncoder;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//
-//    @Test
-//    void signUpTest() {
-//        UserSignupRequestDto requestDto = new UserSignupRequestDto("test@naver.com", "Asdf!234", UserRole.USER);
-//
-//        User user = new User("test@naver.com", "Asdf!234", UserRole.USER);
-//
-//        userService.signUp(requestDto);
-//        userRepository.save(user);
-//
-//        UserSignupResponseDto responseDto = new UserSignupResponseDto(user.getId(), user.getEmail(), user.getRole());
-//
-//        assertEquals(requestDto.getEmail(), responseDto.getEmail());
-//        assertEquals(requestDto.getRole(), responseDto.getRole());
-//
-//
-//    }
-//}
+package org.example.codesix.domain.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.codesix.domain.user.dto.*;
+import org.example.codesix.domain.user.entity.User;
+import org.example.codesix.domain.user.enums.UserRole;
+import org.example.codesix.domain.user.repository.UserRepository;
+import org.example.codesix.domain.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    void signUpTest() {
+        UserSignupRequestDto requestDto = new UserSignupRequestDto("test@naver.com", "Asdf!234", UserRole.USER);
+
+        User user = new User("test@naver.com", "Asdf!234", UserRole.USER);
+
+        userService.signUp(requestDto);
+        userRepository.save(user);
+
+        UserSignupResponseDto responseDto = new UserSignupResponseDto(user.getId(), user.getEmail(), user.getRole());
+
+        assertEquals(requestDto.getEmail(), responseDto.getEmail());
+        assertEquals(requestDto.getRole(), responseDto.getRole());
+
+
+    }
+}
